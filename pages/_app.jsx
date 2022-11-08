87219426aeed8904b0002e74a430bfd7096a5445
shalkauskas <igor.shalkauskas@gmail.com>
2022-11-08 11:01:33 -0500

    add tracker

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
 import "../styles/global.css";
 import "../styles/index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
