534f18a1fe72264e57fe4e975581dd92cbe1cd9a
shalkauskas <ihor@operix.com>
2026-04-27 08:42:21 -0400

    products css refactor

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,44 +1,29 @@
-import React from "react";
-import Image from "next/image";
-import SectionTitle from "./SectionTitle";
+import Image from 'next/image';
+import SectionTitle from './SectionTitle';
+import Accordion from './ui/Accordion/Accordion';
+import styles from './Products.module.css';
+
 export default function Products() {
-  const [brand, setBrandOpen] = React.useState(false);
-  const [green, setGreenOpen] = React.useState(false);
   return (
-    <div className="container mx-auto max-w-screen-xl products">
-      <SectionTitle title={"High-quality products"} />
-      {/* wraper */}
-      <div className="flex flex-wrap flex-row">
-        {/* top brands */}
-        <div className="w-full p-4 sm:w-full md:w-1/2 lg:w-1/2">
-          <div
-            className="mb-8 text-center cursor-pointer sm:cursor-pointer lg:cursor-default md:cursor-default xl:cursor-default"
-            onClick={() => setBrandOpen(!brand)}
-          >
-            <picture>
-              <source type="image/webp" srcSet="top-brands-icon.webp" />
-              <source type="image/png" srcSet="top-brands-icon.png" />
-              <img
-                src="/top-brands-icon.png"
-                alt="Top brands"
-                className="w-10 mr-2 inline-block"
-              />
-            </picture>
-            <h2 className="inline-block">Top brands</h2>
-            <picture>
-              <source type="image/webp" srcSet="arrow.webp" />
-              <source type="image/png" srcSet="arrow.png" />
-              <img
-                src="arrow.png"
-                className={
-                  "inline-block w-6 ml-4 sm:inline-block md:hidden lg:hidden " +
-                  (brand ? "transform rotate-180" : null)
-                }
-              />
-            </picture>
-          </div>
-          <div
-            className={"lg:block md:block " + (brand ? "block mb-6" : "hidden")}
+    <div className={styles.container}>
+      <SectionTitle title="High-quality products" />
+      <div className={styles.wrapper}>
+        <div className={styles.item}>
+          <Accordion
+            title={
+              <>
+                <picture>
+                  <source type="image/webp" srcSet="top-brands-icon.webp" />
+                  <source type="image/png" srcSet="top-brands-icon.png" />
+                  <img
+                    src="/top-brands-icon.png"
+                    alt="Top brands"
+                    className={styles.icon}
+                  />
+                </picture>
+                <h2 className={styles.title}>Top brands</h2>
+              </>
+            }
           >
             <Image
               src="https://res.cloudinary.com/dyj6lkekg/image/upload/tete/top-brands.png"
@@ -46,45 +31,26 @@ export default function Products() {
               width={604}
               height={426}
               quality={75}
-              loading={"eager"}
+              loading="eager"
             />
-          </div>
-          <hr className="block sm:block md:hidden lg:hidden" />
+          </Accordion>
+          <hr className={styles.divider} />
         </div>
-
-        {/* green choices */}
-        <div className="w-full sm:p-4 pt-4 sm:w-full md:w-1/2 lg:w-1/2">
-          <div
-            className="mb-8 text-center cursor-pointer sm:cursor-pointer lg:cursor-default md:cursor-default xl:cursor-default"
-            onClick={() => setGreenOpen(!green)}
-          >
-            <picture>
-              <source type="image/webp" srcSet="green-choices-icon.webp" />
-              <source type="image/png" srcSet="green-choices-icon.png" />
-              <img
-                src="/green-choices-icon.png"
-                alt="Green choices"
-                className="w-10 mr-2 inline-block"
-              />
-            </picture>
-            <h2 className="inline-block">Green choices</h2>
-            <picture>
-              <source type="image/webp" srcSet="arrow.webp" />
-              <source type="image/png" srcSet="arrow.png" />
-              <img
-                src="arrow.png"
-                alt="Open/Hide arrow"
-                className={
-                  "inline-block w-6 ml-4 sm:inline-block md:hidden lg:hidden " +
-                  (green ? "transform rotate-180" : null)
-                }
-              />
-            </picture>
-          </div>
-          <div
-            className={
-              "lg:block md:block " +
-              (green ? "block sm:block" : "hidden sm:hidden")
+        <div className={`${styles.item} ${styles.itemSecond}`}>
+          <Accordion
+            title={
+              <>
+                <picture>
+                  <source type="image/webp" srcSet="green-choices-icon.webp" />
+                  <source type="image/png" srcSet="green-choices-icon.png" />
+                  <img
+                    src="/green-choices-icon.png"
+                    alt="Green choices"
+                    className={styles.icon}
+                  />
+                </picture>
+                <h2 className={styles.title}>Green choices</h2>
+              </>
             }
           >
             <Image
@@ -93,28 +59,11 @@ export default function Products() {
               width={604}
               height={426}
               quality={75}
-              loading={"eager"}
+              loading="eager"
             />
-          </div>
+          </Accordion>
         </div>
       </div>
-      <style jsx>{`
-        h2 {
-          font-size: 24px;
-          line-height: 29px;
-          color: #4d4949;
-        }
-        @media screen and (max-width: 1400px) {
-          .products {
-            max-width: 90%;
-          }
-        }
-        @media screen and (max-width: 1024px) {
-          .products {
-            max-width: 86%;
-          }
-        }
-      `}</style>
     </div>
   );
 }
